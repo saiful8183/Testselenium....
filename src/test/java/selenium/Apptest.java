9a27cfad0e9ba8cd44469776d2657d4c8f291caa
saiful8183 <Manha103019@gmail.com>
2023-01-20 05:08:27 -0500

    My 11th project

--- src/test/java/selenium/Apptest.java
+++ src/test/java/selenium/Apptest.java
@@ -1,17 +1,32 @@
 package selenium;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import sdet.com.App;
 
 public class Apptest {
 
-	public static void main(String[] args) {
-		
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\Saiful Chowdhury\\OneDrive\\Desktop\\Automation\\chromedriver.exe");	
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.facebook.com/signup");
+	  @Test
+	public void testlogin1()
+
+	{
+	    App myapp=new App();
+	    Assert.assertEquals(0,myapp.userLogin("abc","abc123"));
+	}
+
+
+
+	    @Test
+	public void testlogin2()
+
+	{
+	App myapp=new App();
+	Assert.assertEquals(1,myapp.userLogin("abc","abc@123"));
+	}
+
 
 	}
 
-}
+
---
+++ src/test/java/selenium/TC_login.java
@@ -0,0 +1,38 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_login {
+
+	
+	
+	@Test
+	
+	public void login_Test()
+	
+	{
+	
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	
+	driver.manage().window().maximize();
+	
+	driver.get("https://www.saucedemo.com/");
+	
+	
+	driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	driver.findElement(By.id("login-button")).click();
+	
+	
+	Assert.assertEquals(driver.getTitle(),"Swag Labs");
+	
+	
+	}
+	}
